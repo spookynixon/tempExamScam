013e7988d8dae8b051f3b72261545b519128a756
Steve S <a@b.com>
2011-05-30 16:02:45 +0100

    updated formatting

--- src/com/examscam/model/Foobar.java
+++ src/com/examscam/model/Foobar.java
@@ -27,42 +27,46 @@ public class Foobar
 	@Id
 	@GeneratedValue
 	private Long id;
-	
+
 	private String fooName;
-	
+
 	@Column(table = "bar")
 	private String barCode;
-	
-	
-	
-	
+
+
 	public void setId(Long id)
 	{
 		this.id = id;
 	}
+
+
 	public Long getId()
 	{
 		return id;
 	}
+
+
 	public void setFooName(String fooName)
 	{
 		this.fooName = fooName;
 	}
+
+
 	public String getFooName()
 	{
 		return fooName;
 	}
+
+
 	public void setBarCode(String barCode)
 	{
 		this.barCode = barCode;
 	}
+
+
 	public String getBarCode()
 	{
 		return barCode;
 	}
-
-
-
-
 
 }
